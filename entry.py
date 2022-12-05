23513567578ed7c946881ac109398cb37397665e
Ivan@OMEN <ivan.xiangyf@outlook.com>
2022-12-06 06:18:46 +0800

    lasso done, correlation done

---
+++ entry.py
@@ -0,0 +1,29 @@
+a = [] #list array
+a = [1,2,3,4] #index 0, 1, 2, 3
+print(a[1])
+
+# dictionary
+
+d = {'age':10, 'name': 'Ivan', 'weight': 60}
+print(d['age'])
+
+# loop
+# for, while
+
+# 0 - 10 [)
+for i in range(11):
+    pass # do sth
+
+# for i in range(1000, 10001) #(1000-10000)
+
+for i in range(len(a)):
+    print(a[i])
+
+for item in a:
+    print(item)
+
+for i in d.items():
+    print(i)
+
+#scikit learn
+
